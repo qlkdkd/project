d3c32f7a5fbb13e19d736f2926d811027bd77860
신대한 <71871927+qlkdkd@users.noreply.github.com>
2025-07-18 10:38:35 +0900

    Delete erp_project_1 directory

--- erp_project_1/inventory/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.inventory_list, name='inventory_list'),
-    path('add/<int:item_id>/', views.add_stock, name='add_stock'),
-    path('remove/<int:item_id>/', views.remove_stock, name='remove_stock'),
-]
